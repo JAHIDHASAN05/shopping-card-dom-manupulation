c7d9ed06a37fa654e5a2f8ec23f9a9f1f9e398d3
JAHID HASAN <developerjahid@gmail.com>
2023-10-20 19:38:39 +0600

    update phone plus minus button and calculating total phone price

---
+++ js/phone.js
@@ -0,0 +1,38 @@
+
+function updatePhoneNumber(isIncrease){
+  const phoneNumberField=document.getElementById("phone-number-field")
+  const phoneNumberString=phoneNumberField.value ;
+  const previousPhoneNumber=parseInt(phoneNumberString);
+  let newPhoneNumber;
+  if(isIncrease===true){
+     newPhoneNumber=previousPhoneNumber+1;
+  }
+  else{
+    newPhoneNumber=previousPhoneNumber-1;
+
+  }
+  phoneNumberField.value=newPhoneNumber;
+  return newPhoneNumber;
+}
+
+
+function updatePhoneTotalPrice(newPhoneNumber){
+  const newPhoneTotalPrice =newPhoneNumber*1219;
+  const phonefinalPrice=document.getElementById("phone-total-price")
+  phonefinalPrice.innerText=newPhoneTotalPrice;
+}
+
+
+document.getElementById("btn-phone-plus").addEventListener("click", function(){
+
+ const newPhoneNumber=updatePhoneNumber(true)
+ updatePhoneTotalPrice(newPhoneNumber)
+
+ 
+})
+
+document.getElementById("btn-phone-minus").addEventListener("click", function(){
+ const newPhoneNumber= updatePhoneNumber(false)
+ updatePhoneTotalPrice(newPhoneNumber)
+
+})
