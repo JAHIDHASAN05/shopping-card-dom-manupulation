0723caeabc1451fd429edcb9ccbcc0f165e66cdb
JAHID HASAN <developerjahid@gmail.com>
2023-10-20 18:37:32 +0600

    casing plus minus and total price funtional

---
+++ js/case.js
@@ -0,0 +1,35 @@
+function caseNumberField(isIncrease){
+    const caseNumberField=document.getElementById("case-number-field")
+    const caseNumberString=caseNumberField.value ;
+    const caseNumber= parseFloat(caseNumberString);
+
+    let newCaseNumber;
+
+    if(isIncrease===true){
+         newCaseNumber=caseNumber+1;
+    }
+    else{
+         newCaseNumber=caseNumber-1;
+    }
+
+    
+    caseNumberField.value=newCaseNumber;
+
+    return newCaseNumber;
+}
+
+
+document.getElementById("btn-case-plus").addEventListener("click", function(){
+    const  newCaseNumber= caseNumberField(true);
+    const newCasePrice=newCaseNumber*59;
+    const caseTotalPrice=document.getElementById("case-total-price")
+    caseTotalPrice.innerText=newCasePrice;
+     
+})
+
+document.getElementById("btn-case-minus").addEventListener("click",function(){
+    const  newCaseNumber= caseNumberField(false);
+     const newCasePrice=newCaseNumber*59;
+     const caseTotalPrice=document.getElementById("case-total-price")
+     caseTotalPrice.innerText=newCasePrice;
+})
